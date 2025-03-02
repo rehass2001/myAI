16480a96403786a803faf9b8413dbfbd2dcfefda
rehass2001 <126594835+rehass2001@users.noreply.github.com>
2025-03-02 14:31:24 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -2,7 +2,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `BeatSync`;
 export const MESSAGE_PLACEHOLDER: string = `Tell me your mood, and I'll find the perfect jam!`;
-export const FOOTER_MESSAGE: string = `Musice AI By Raheq Hassan`;
+export const FOOTER_MESSAGE: string = `Music AI By Raheq Hassan`;
 export const CLEAR_BUTTON_TEXT: string = `Start Fresh 🎶`;
 export const PAGE_TITLE: string = `Your Personal DJ AI`;
 export const PAGE_DESCRIPTION: string = `Hello! What’s the mood today? Let’s find the perfect soundtrack!`;
