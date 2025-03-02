092d7c753c21903f7f45e172f7df3ac9891127ca
rehass2001 <126594835+rehass2001@users.noreply.github.com>
2025-03-02 15:25:36 -0500

    Update response.ts

--- modules/response.ts
+++ modules/response.ts
@@ -49,6 +49,94 @@ import {
  * ResponseModule is responsible for collecting data and building a response
  */
 export class ResponseModule {
+  static async respondToRandomMessage(
+    chat: Chat,
+    providers: AIProviders
+  ): Promise<Response> {
+    /**
+     * Respond to the user when they send a RANDOM message
+     */
+    const PROVIDER_NAME: ProviderName = RANDOM_RESPONSE_PROVIDER;
+    const MODEL_NAME: string = RANDOM_RESPONSE_MODEL;
+
+    const stream = new ReadableStream({
+      async start(controller) {
+        queueIndicator({
+          controller,
+          status: "Coming up with an answer",
+          icon: "thinking",
+        });
+        const systemPrompt = RESPOND_TO_RANDOM_MESSAGE_SYSTEM_PROMPT();
+        const mostRecentMessages: CoreMessage[] = await convertToCoreMessages(
+          stripMessagesOfCitations(chat.messages.slice(-HISTORY_CONTEXT_LENGTH))
+        );
+
+        const citations: Citation[] = [];
+        queueAssistantResponse({
+          controller,
+          providers,
+          providerName: PROVIDER_NAME,
+          messages: mostRecentMessages,
+          model_name: MODEL_NAME,
+          systemPrompt,
+          citations,
+          error_message: DEFAULT_RESPONSE_MESSAGE,
+          temperature: RANDOM_RESPONSE_TEMPERATURE,
+        });
+      },
+    });
+
+    return new Response(stream, {
+      headers: {
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        Connection: "keep-alive",
+      },
+    });
+  }
+
+  static async respondToHostileMessage(
+    chat: Chat,
+    providers: AIProviders
+  ): Promise<Response> {
+    /**
+     * Respond to the user when they send a HOSTILE message
+     */
+    const PROVIDER_NAME: ProviderName = HOSTILE_RESPONSE_PROVIDER;
+    const MODEL_NAME: string = HOSTILE_RESPONSE_MODEL;
+
+    const stream = new ReadableStream({
+      async start(controller) {
+        queueIndicator({
+          controller,
+          status: "Coming up with an answer",
+          icon: "thinking",
+        });
+        const systemPrompt = RESPOND_TO_HOSTILE_MESSAGE_SYSTEM_PROMPT();
+        const citations: Citation[] = [];
+        queueAssistantResponse({
+          controller,
+          providers,
+          providerName: PROVIDER_NAME,
+          messages: [],
+          model_name: MODEL_NAME,
+          systemPrompt,
+          citations,
+          error_message: DEFAULT_RESPONSE_MESSAGE,
+          temperature: HOSTILE_RESPONSE_TEMPERATURE,
+        });
+      },
+    });
+
+    return new Response(stream, {
+      headers: {
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        Connection: "keep-alive",
+      },
+    });
+  }
+
   static async respondToQuestion(
     chat: Chat,
     providers: AIProviders,
