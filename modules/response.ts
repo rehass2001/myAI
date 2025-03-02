03243bb389370b8ab67e35682273a49b083d7eaa
rehass2001 <126594835+rehass2001@users.noreply.github.com>
2025-03-02 16:22:23 -0500

    Update response.ts

--- modules/response.ts
+++ modules/response.ts
@@ -1,17 +1,35 @@
 import {
   Chat,
+  Chunk,
+  Source,
+  CoreMessage,
   AIProviders,
   ProviderName,
+  Citation,
 } from "@/types";
 import {
+  convertToCoreMessages,
+  embedHypotheticalData,
+  generateHypotheticalData,
+  getSourcesFromChunks,
+  searchForChunksUsingEmbedding,
+  getContextFromSources,
+  getCitationsFromChunks,
+  buildPromptFromContext,
+} from "@/utilities/chat";
+import {
   queueAssistantResponse,
+  queueError,
   queueIndicator,
 } from "@/actions/streaming";
 import {
+  HISTORY_CONTEXT_LENGTH,
   DEFAULT_RESPONSE_MESSAGE,
 } from "@/configuration/chat";
+import { stripMessagesOfCitations } from "@/utilities/chat";
 import {
   RESPOND_TO_HOSTILE_MESSAGE_SYSTEM_PROMPT,
+  RESPOND_TO_QUESTION_BACKUP_SYSTEM_PROMPT,
   RESPOND_TO_QUESTION_SYSTEM_PROMPT,
   RESPOND_TO_RANDOM_MESSAGE_SYSTEM_PROMPT,
 } from "@/configuration/prompts";
@@ -31,6 +49,52 @@ import {
  * ResponseModule is responsible for collecting data and building a response
  */
 export class ResponseModule {
+  static async respondToRandomMessage(
+    chat: Chat,
+    providers: AIProviders
+  ): Promise<Response> {
+    /**
+     * Respond to the user when they send a RANDOM message
+     */
+    const PROVIDER_NAME: ProviderName = RANDOM_RESPONSE_PROVIDER;
+    const MODEL_NAME: string = RANDOM_RESPONSE_MODEL;
+
+    const stream = new ReadableStream({
+      async start(controller) {
+        queueIndicator({
+          controller,
+          status: "Coming up with an answer",
+          icon: "thinking",
+        });
+        const systemPrompt = RESPOND_TO_RANDOM_MESSAGE_SYSTEM_PROMPT();
+        const mostRecentMessages: CoreMessage[] = await convertToCoreMessages(
+          stripMessagesOfCitations(chat.messages.slice(-HISTORY_CONTEXT_LENGTH))
+        );
+
+        const citations: Citation[] = [];
+        queueAssistantResponse({
+          controller,
+          providers,
+          providerName: PROVIDER_NAME,
+          messages: mostRecentMessages,
+          model_name: MODEL_NAME,
+          systemPrompt,
+          citations,
+          error_message: DEFAULT_RESPONSE_MESSAGE,
+          temperature: RANDOM_RESPONSE_TEMPERATURE,
+        });
+      },
+    });
+
+    return new Response(stream, {
+      headers: {
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        Connection: "keep-alive",
+      },
+    });
+  }
+
   static async respondToHostileMessage(
     chat: Chat,
     providers: AIProviders
@@ -49,6 +113,7 @@ export class ResponseModule {
           icon: "thinking",
         });
         const systemPrompt = RESPOND_TO_HOSTILE_MESSAGE_SYSTEM_PROMPT();
+        const citations: Citation[] = [];
         queueAssistantResponse({
           controller,
           providers,
@@ -56,7 +121,7 @@ export class ResponseModule {
           messages: [],
           model_name: MODEL_NAME,
           systemPrompt,
-          citations: [],
+          citations,
           error_message: DEFAULT_RESPONSE_MESSAGE,
           temperature: HOSTILE_RESPONSE_TEMPERATURE,
         });
@@ -74,7 +139,8 @@ export class ResponseModule {
 
   static async respondToQuestion(
     chat: Chat,
-    providers: AIProviders
+    providers: AIProviders,
+    index: any
   ): Promise<Response> {
     /**
      * Respond to the user when they send a QUESTION
@@ -82,29 +148,85 @@ export class ResponseModule {
     const PROVIDER_NAME: ProviderName = QUESTION_RESPONSE_PROVIDER;
     const MODEL_NAME: string = QUESTION_RESPONSE_MODEL;
 
-    const systemPrompt = RESPOND_TO_QUESTION_SYSTEM_PROMPT();
-    
-    return new Response(JSON.stringify({ message: "Processing question." }), {
-      headers: { "Content-Type": "application/json" },
+    const stream = new ReadableStream({
+      async start(controller) {
+        queueIndicator({
+          controller,
+          status: "Figuring out what your answer looks like",
+          icon: "thinking",
+        });
+        try {
+          const hypotheticalData: string = await generateHypotheticalData(
+            chat,
+            providers.openai
+          );
+          const { embedding }: { embedding: number[] } =
+            await embedHypotheticalData(hypotheticalData, providers.openai);
+          queueIndicator({
+            controller,
+            status: "Reading through documents",
+            icon: "searching",
+          });
+          const chunks: Chunk[] = await searchForChunksUsingEmbedding(
+            embedding,
+            index
+          );
+          const sources: Source[] = await getSourcesFromChunks(chunks);
+          queueIndicator({
+            controller,
+            status: `Read over ${sources.length} documents`,
+            icon: "documents",
+          });
+          const citations: Citation[] = await getCitationsFromChunks(chunks);
+          const contextFromSources = await getContextFromSources(sources);
+          const systemPrompt =
+            RESPOND_TO_QUESTION_SYSTEM_PROMPT(contextFromSources);
+          queueIndicator({
+            controller,
+            status: "Coming up with an answer",
+            icon: "thinking",
+          });
+          queueAssistantResponse({
+            controller,
+            providers,
+            providerName: PROVIDER_NAME,
+            messages: stripMessagesOfCitations(
+              chat.messages.slice(-HISTORY_CONTEXT_LENGTH)
+            ),
+            model_name: MODEL_NAME,
+            systemPrompt,
+            citations,
+            error_message: DEFAULT_RESPONSE_MESSAGE,
+            temperature: QUESTION_RESPONSE_TEMPERATURE,
+          });
+        } catch (error: any) {
+          console.error("Error in respondToQuestion:", error);
+          queueError({
+            controller,
+            error_message: error.message ?? DEFAULT_RESPONSE_MESSAGE,
+          });
+          queueAssistantResponse({
+            controller,
+            providers,
+            providerName: PROVIDER_NAME,
+            messages: [],
+            model_name: MODEL_NAME,
+            systemPrompt: RESPOND_TO_QUESTION_BACKUP_SYSTEM_PROMPT(),
+            citations: [],
+            error_message: DEFAULT_RESPONSE_MESSAGE,
+            temperature: QUESTION_RESPONSE_TEMPERATURE,
+          });
+        }
+      },
     });
-  }
 
-  static async respondToRandomMessage(
-    chat: Chat,
-    providers: AIProviders
-  ): Promise<Response> {
-    /**
-     * Respond to the user when they send a RANDOM message
-     */
-    const PROVIDER_NAME: ProviderName = RANDOM_RESPONSE_PROVIDER;
-    const MODEL_NAME: string = RANDOM_RESPONSE_MODEL;
-
-    const systemPrompt = RESPOND_TO_RANDOM_MESSAGE_SYSTEM_PROMPT();
-    
-    return new Response(JSON.stringify({ message: "Processing random message." }), {
-      headers: { "Content-Type": "application/json" },
+    return new Response(stream, {
+      headers: {
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        Connection: "keep-alive",
+      },
     });
   }
 }
-
 
