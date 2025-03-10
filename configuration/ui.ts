47a3af5490dc06512cb8823fd4e98d2994730890
rehass2001 <126594835+rehass2001@users.noreply.github.com>
2025-03-10 13:19:12 -0400

    Update ui.ts 2

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,9 +1,9 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `VibeTune`;
+export const CHAT_HEADER: string = `MealChef`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI By Raheq Hassan`;
-export const CLEAR_BUTTON_TEXT: string = `Clear Chat for New Songs`;
+export const CLEAR_BUTTON_TEXT: string = `Clear Chat for New Recipes`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
