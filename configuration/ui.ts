9bc88b1666c92728cfd7f04d089db4268b05801b
rehass2001 <126594835+rehass2001@users.noreply.github.com>
2025-03-10 14:46:44 -0400

    Update ui.ts 4

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `ChefMate`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI By Raheq Hassan`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Chat for New Recipes`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `ChefMate`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
